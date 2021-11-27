fe18392d546ebb8fd940920259e9a8d935eb4e71
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-11-27 19:11:06 +0300

    SV-28 Add theme switcher

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { HeaderBrandLink } from "./components/HeaderBrandLink";
 import { HeaderLogo } from "./components/HeaderLogo";
+import { HeaderThemeSwitcher } from "./components/HeaderThemeSwitcher";
 
 type HeaderProps = {};
 
 export const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+    <div className="navbar mb-3 shadow-lg bg-neutral text-neutral-content">
       <HeaderLogo />
+
+      <HeaderThemeSwitcher />
       <HeaderBrandLink
         brand={"github"}
         link={"https://github.com/miet-students/sorting-visualizer/"}
