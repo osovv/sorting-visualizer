913607d653b2585478f22c832c3cdfdbd95798b8
Daniil <antipov8v@gmail.com>
2021-12-21 21:17:13 +0300

    SV-25 Create Button

---
+++ src/components/common/button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ButtonProps = {
+    id: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    label: string;
+};
+
+export const Button: React.FC<ButtonProps> = ({onClick, id, label}) => {
+return (
+    <button className="btn primary btn-sm" onClick={onClick} id={id}>
+    {label}
+    </button>
+);
+}
