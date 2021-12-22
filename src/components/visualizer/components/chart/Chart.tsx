90a6fc274684b8a840064bc4c049fb1438469205
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-22 18:05:23 +0300

    SV-9 Create visualizer chart component

---
+++ src/components/visualizer/components/chart/Chart.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { SortHistoryStep, ElementStatus } from "../../../../types";
+import { Bar } from "./components/bar/Bar";
+
+type Props = {
+  max: number;
+  sortHistorySteps: SortHistoryStep[];
+  step: number;
+};
+
+const getElementStatus = (index: number, sortHistoryStep: SortHistoryStep) => {
+  let barStatus: ElementStatus;
+  if (sortHistoryStep.swapping.includes(index)) {
+    barStatus = "swapping";
+  } else if (sortHistoryStep.sorted.includes(index)) {
+    barStatus = "sorted";
+  } else if (sortHistoryStep.comparing.includes(index)) {
+    barStatus = "comparing";
+  } else barStatus = "waiting";
+  return barStatus;
+};
+
+export const Chart: React.FC<Props> = ({ max, sortHistorySteps, step }) => {
+  const array = sortHistorySteps[step].array;
+  const size = array.length;
+  return (
+    <div className="bg-base-300 w-auto h-[80vh] m-4 mt-1 flex flex-row items-end p-4">
+      {array.map((value, index) => {
+        const width = 100 / size;
+        const height = (value / max) * 100;
+
+        const marginRight = index === size ? "mr-0" : "mr-[0.1rem]";
+        const className = marginRight;
+
+        const status = getElementStatus(index, sortHistorySteps[step]);
+
+        return (
+          <Bar
+            width={width}
+            height={height}
+            className={className}
+            key={index}
+            status={status}
+          />
+        );
+      })}
+    </div>
+  );
+};
