1b4f3dff09fa99febf59ba78cd94384c46cef771
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-22 17:57:32 +0300

    SV-9 Create bubble sort algorithm

---
+++ src/algorithms/bubble_sort/index.ts
@@ -0,0 +1,28 @@
+import { SortHistoryStep } from "../../types";
+import { swapUnsafe } from "../../utils/array";
+import {
+  addToComparing,
+  addToSorted,
+  addToSwapping,
+  cleanStatuses,
+  initializeSteps,
+} from "../helpers";
+
+export const BubbleSort = (array: number[]): SortHistoryStep[] => {
+  let nums = array.slice();
+  let historySteps: SortHistoryStep[] = initializeSteps(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      historySteps = addToComparing(historySteps, j, j + 1);
+      if (nums[j] > nums[j + 1]) {
+        nums = swapUnsafe(nums, j, j + 1);
+        historySteps = addToSwapping(historySteps, j, j + 1);
+      }
+      historySteps = cleanStatuses(historySteps);
+    }
+    historySteps = addToSorted(historySteps, nums.length - i - 1);
+  }
+
+  return historySteps;
+};
