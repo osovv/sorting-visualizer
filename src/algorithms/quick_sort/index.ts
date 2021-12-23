dd67a059ce29bb40c0320ab4e4e6a6955afb376a
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-23 23:50:05 +0300

    SV-40 Fix main rebase

--- src/algorithms/quick_sort/index.ts
+++ src/algorithms/quick_sort/index.ts
@@ -1,5 +1,4 @@
 import { SortHistoryStep } from "../../types";
-//import { swapUnsafe } from "../../utils/array";
 import {
   addToComparing,
   addToSorted,
@@ -8,79 +7,69 @@ import {
   initializeSteps,
 } from "../helpers";
 
-
 function addToSortedRules(
-  historySteps: SortHistoryStep[], 
-  left: number, 
+  historySteps: SortHistoryStep[],
+  left: number,
   right: number,
-  pointer: number 
-){
-  if ((pointer == left + 1) && (right - left !== 2)) {
-    historySteps = addToSorted(historySteps, pointer);
-    historySteps = addToSorted(historySteps, left);
-  }
-  else if (right - left === 2) {
-    historySteps = addToSorted(historySteps, right);
-    historySteps = addToSorted(historySteps, right - 1);
-    historySteps = addToSorted(historySteps, left);
-  }
-  else if ((pointer === right - 1)){
-    historySteps = addToSorted(historySteps, right);
-    historySteps = addToSorted(historySteps, pointer);
-  }
-  else{
-    historySteps = addToSorted(historySteps, pointer);
+  pointer: number
+) {
+  if (pointer == left + 1 && right - left !== 2) {
+    historySteps = addToSorted(historySteps, [pointer, left]);
+  } else if (right - left === 2) {
+    historySteps = addToSorted(historySteps, [right, right - 1, left]);
+  } else if (pointer === right - 1) {
+    historySteps = addToSorted(historySteps, [right, pointer]);
+  } else {
+    historySteps = addToSorted(historySteps, [pointer]);
   }
 
   return historySteps;
 }
 
 function partition(
-  historySteps: SortHistoryStep[], 
-  nums: number[], 
-  left: number, 
-  right: number, 
-):[number, SortHistoryStep[]]{
+  historySteps: SortHistoryStep[],
+  nums: number[],
+  left: number,
+  right: number
+): [number, SortHistoryStep[]] {
   const pivot = nums[right];
   let i = left - 1;
 
-  for (let j = left; j <= right - 1; j++){
+  for (let j = left; j <= right - 1; j++) {
     historySteps = addToComparing(historySteps, j, right);
-    if (nums[j] <= pivot){
+    if (nums[j] <= pivot) {
       i++;
-      if (i !== j){
+      if (i !== j) {
         historySteps = addToSwapping(historySteps, i, j);
       }
-      
+
       [nums[i], nums[j]] = [nums[j], nums[i]];
-      //nums = swapUnsafe(nums, j, i);
     }
     historySteps = cleanStatuses(historySteps);
   }
   historySteps = addToSwapping(historySteps, right, i + 1);
-  
+
   [nums[i + 1], nums[right]] = [nums[right], nums[i + 1]];
-  //nums = swapUnsafe(nums, right, i + 1);
-  
+
   historySteps = cleanStatuses(historySteps);
   historySteps = addToSortedRules(historySteps, left, right, i + 1);
 
-  return [(i + 1), historySteps]
+  return [i + 1, historySteps];
 }
 
 function quickSortRecursion(
-  historySteps: SortHistoryStep[], 
-  nums: number[], 
-  left: number, 
+  historySteps: SortHistoryStep[],
+  nums: number[],
+  left: number,
   right: number
-){
-  if (left < right){
+) {
+  if (left < right) {
     let index;
     [index, historySteps] = partition(historySteps, nums, left, right);
     historySteps = quickSortRecursion(historySteps, nums, left, index - 1);
     historySteps = quickSortRecursion(historySteps, nums, index + 1, right);
-  }   
-  
+  }
+
   return historySteps;
 }
 
@@ -91,7 +80,6 @@ export const QuickSort = (array: number[]): SortHistoryStep[] => {
   console.log(historySteps);
 
   historySteps = quickSortRecursion(historySteps, nums, 0, nums.length - 1);
-  
-  console.log(historySteps);
+
   return historySteps;
 };
