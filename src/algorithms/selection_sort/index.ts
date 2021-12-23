5bc7a26e29f1e8511d13b55c831465bfc8e66f9f
Daniil <antipov8v@gmail.com>
2021-12-23 23:25:38 +0300

    SV-43 Add selection sort

---
+++ src/algorithms/selection_sort/index.ts
@@ -0,0 +1,31 @@
+import { SortHistoryStep } from "../../types";
+import { swapUnsafe } from "../../utils/array";
+import {
+  addToComparing,
+  addToSorted,
+  addToSwapping,
+  cleanStatuses,
+  initializeSteps,
+} from "../helpers";
+
+export const SelectionSort = (array: number[]): SortHistoryStep[] => {
+  let nums = array.slice();
+  let historySteps: SortHistoryStep[] = initializeSteps(nums);
+
+  for (let i = 0; i < nums.length - 1; i++) {
+      let min = i;
+      for(let j = i + 1; j < nums.length; j++) {
+        historySteps = addToComparing(historySteps, j, min);
+        if ( nums[j] < nums[min] ) {
+          min = j;
+        }
+      }
+      nums = swapUnsafe(nums, i, min);
+      historySteps = addToSwapping(historySteps, i, min);
+      historySteps = cleanStatuses(historySteps);
+      historySteps = addToSorted(historySteps, i);
+  }
+  historySteps = addToSorted(historySteps,  nums.length - 1);
+
+  return historySteps;
+};
