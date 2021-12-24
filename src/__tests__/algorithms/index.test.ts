25544c6baa35dfc2dac3cf07ccbfcab04c49e95e
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-24 17:25:22 +0300

    SV-50 Add tests for sorting algorithms

---
+++ src/__tests__/algorithms/index.test.ts
@@ -0,0 +1,34 @@
+import * as sorts from "../../algorithms";
+import { generateRandomArray, getLast, getLastUnsafe } from "../../utils/array";
+
+const ARRAY = generateRandomArray(10, 0, 100);
+
+describe("sorting algortihmm", () => {
+  describe("should return non-empty sorting history", () => {
+    for (const sortKey in sorts) {
+      it(`${sortKey}`, () => {
+        const sort = sorts[sortKey];
+        const history = sort(ARRAY);
+        expect(history.length).toBeGreaterThan(0);
+      });
+    }
+  });
+
+  describe("should return sorted original array on last step", () => {
+    function _sort(array: number[]) {
+      let arr = array.slice();
+      arr.sort((a, b) => a - b);
+      return arr;
+    }
+
+    for (const sortKey in sorts) {
+      it(`${sortKey}`, () => {
+        const sort = sorts[sortKey];
+        const history = sort(ARRAY);
+        const lastStep = getLastUnsafe(history);
+        const resultArray = lastStep.array;
+        expect(resultArray).toEqual(_sort(ARRAY));
+      });
+    }
+  });
+});
