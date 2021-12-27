1039d67b65aa6e2308e1ed1313f3b2f70497b526
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-27 09:02:45 +0300

    SV-50 Add tests for Button

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,9 @@
+import { configure } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 import { toBeWithin } from "jest-extended";
+
+configure({
+  testIdAttribute: "id",
+});
 
 expect.extend({ toBeWithin });
