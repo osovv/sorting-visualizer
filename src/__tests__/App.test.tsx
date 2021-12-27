60d9e53a75a85087f5b58c8452c276d63aab5b40
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-27 09:16:58 +0300

    SV-50 Add simple integration test

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "../App";
+import { getRandomNumber } from "../utils/array";
+
+it("should render same amount of bars as in array size input", () => {
+  const size = getRandomNumber(10, 100);
+
+  render(<App />);
+  const slider = screen.getByTestId("size_slider");
+  const chart = screen.getByTestId("chart");
+  const bars = chart.children;
+
+  fireEvent.change(slider, { target: { value: String(size) } });
+
+  expect(bars.length).toEqual(size);
+});
