675da746a50a09f90d739ad270c34f10c5a8b737
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-07-23 11:43:08 +0300

    Rename files

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,8 +1,8 @@
-import { __BubbleSort as BubbleSort } from "./bubble_sort";
-import { __CocktailShakerSort as CocktailShakerSort } from "./cocktail_shaker_sort";
-import { __InsertionSort as InsertionSort } from "./insertion_sort";
-import { __SelectionSort as SelectionSort } from "./selection_sort";
-import { __QuickSort as QuickSort } from "./quick_sort";
+import { __BubbleSort as BubbleSort } from "./bubble_sort/bubble_sort";
+import { __CocktailShakerSort as CocktailShakerSort } from "./cocktail_shaker_sort/cocktail_shaker";
+import { __InsertionSort as InsertionSort } from "./insertion_sort/insertion_sort";
+import { __SelectionSort as SelectionSort } from "./selection_sort/selection_sort";
+import { __QuickSort as QuickSort } from "./quick_sort/quick_sort";
 
 export {
   BubbleSort,
