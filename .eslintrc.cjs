875d328e1527e0ffc81e38d4cd61a8ef798eaf81
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-10-03 16:39:59 +0300

    Refactor /src/app

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
