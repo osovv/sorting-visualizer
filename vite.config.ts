4ead10cfe60eac5e8f8584e283f695a0436be65b
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-10-03 14:59:33 +0300

    Install vitest/coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  test: { environment: 'jsdom' },
+  test: {
+    environment: 'jsdom',
+  },
   plugins: [react(), tsconfigPaths()],
 });
