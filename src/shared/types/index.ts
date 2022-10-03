0eb6f32c79235a3f8a3e04427e36c2041fde3417
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-10-03 21:48:58 +0300

    Remove AppState type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,14 +1,5 @@
 import { SortHistory } from 'entities/sort_history';
 
-export type AppState = {
-  array: number[];
-  min: number;
-  max: number;
-  size: number;
-  delayMs: number;
-  sort: ((ar: number[]) => SortHistory) | undefined;
-};
-
 export type SortMapping = {
   name: string;
   value: (_array: number[]) => SortHistory;
