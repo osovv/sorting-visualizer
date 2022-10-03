c63bfd663df213ad391a662b77c15ad6e3a68def
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-10-03 13:39:55 +0300

    Install vite, eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+};
