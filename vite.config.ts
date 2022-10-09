50574cc6a43d0c8fc6a63e1795336f2a0cd308f5
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-10-09 08:24:29 +0300

    Fix Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const gh_pages = process.env.GH_PAGES;
+
 export default defineConfig({
   test: {
     environment: 'jsdom',
   },
   plugins: [react(), tsconfigPaths()],
-  base: '/sorting-visualizer/',
+  base: gh_pages ? '/sorting-visualizer/' : '/',
 });
